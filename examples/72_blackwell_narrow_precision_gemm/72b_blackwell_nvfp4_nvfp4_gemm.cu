bac93634cc7fbc9367d2941f4dbf3e911c48ee96
liz <lzw20160910@sina.com>
2026-08-18 17:24:49 +0800

    ...

--- examples/72_blackwell_narrow_precision_gemm/72b_blackwell_nvfp4_nvfp4_gemm.cu
+++ examples/72_blackwell_narrow_precision_gemm/72b_blackwell_nvfp4_nvfp4_gemm.cu
@@ -48,12 +48,10 @@ using         LayoutBTag  = cutlass::layout::ColumnMajor;                   // L
 constexpr int AlignmentB  = 32;                                             // Memory access granularity/alignment of B matrix in units of elements (up to 16 bytes)
 
 // C/D matrix configuration
-using         ElementD    = cutlass::float_e2m1_t;                          // Element type for D matrix operand
-using         ElementSFD  = cutlass::float_ue8m0_t;                         // Element type for SFB matrix operand
+using         ElementD    = cutlass::half_t;                                // Element type for D matrix operand
 using         ElementC    = float;                                          // Element type for C matrix operand
 using         LayoutCTag  = cutlass::layout::RowMajor;                      // Layout type for C matrix operand
 using         LayoutDTag  = cutlass::layout::RowMajor;                      // Layout type for D matrix operand
-using         LayoutSFDTag = LayoutDTag;                                    // Layout type for SFD should be same as D matrix operand
 
 constexpr int AlignmentD  = 128 / cutlass::sizeof_bits<ElementD>::value;    // Memory access granularity/alignment of C matrix in units of elements (up to 16 bytes)
 constexpr int AlignmentC  = 128 / cutlass::sizeof_bits<ElementC>::value;    // Memory access granularity/alignment of C matrix in units of elements (up to 16 bytes)
@@ -69,16 +67,13 @@ using MmaTileShape        = Shape<_128,_128,_256>;                          // M
 using ClusterShape        = Shape<_1,_1,_1>;                                // Shape of the threadblocks in a cluster
 
 constexpr int InputSFVectorSize  = 16;
-constexpr int OutputSFVectorSize = InputSFVectorSize;
 
 // D = alpha * acc + beta * C
-//      With BlockScaleFactor generation.
-using FusionOperation = cutlass::epilogue::fusion::LinCombBlockScaleFactor<
-    OutputSFVectorSize,
-    ElementD,
-    ElementCompute,
-    ElementSFD, LayoutSFDTag,
-    ElementC>;
+using FusionOperation = cutlass::epilogue::fusion::LinearCombination<
+    ElementD,      // FP16
+    ElementCompute, // float
+    ElementC        // float
+>;
 
 using CollectiveEpilogue = typename cutlass::epilogue::collective::CollectiveBuilder<
     ArchTag, OperatorClass,
@@ -121,10 +116,6 @@ using LayoutC   = decltype(cute::make_layout(make_shape(0,0,0), StrideC{}));
 using StrideD   = typename Gemm::GemmKernel::StrideD;
 using LayoutD   = decltype(cute::make_layout(make_shape(0,0,0), StrideD{}));
 
-using FusionOp = typename Gemm::EpilogueOutputOp;
-constexpr bool IsBlockScaleSupported = FusionOp::IsBlockScaleSupported;
-using SfdOutputCfg = cutlass::detail::Sm1xxBlockScaledOutputConfig<OutputSFVectorSize>;
-using LayoutSFD = typename SfdOutputCfg::LayoutSF;
 
 //
 // Data members
@@ -141,7 +132,6 @@ StrideC stride_C;
 LayoutC layout_C;
 StrideD stride_D;
 LayoutD layout_D;
-LayoutSFD layout_SFD;
 
 uint64_t seed;
 
@@ -154,12 +144,10 @@ cutlass::HostTensor<ElementB::ScaleFactorType, cutlass::layout::PackedVectorLayo
 cutlass::HostTensor<ElementC, cutlass::layout::PackedVectorLayout> block_C;              //fp32
 // Output Tensors
 cutlass::HostTensor<ElementD, cutlass::layout::PackedVectorLayout> block_D;              //nvfp4
-cutlass::HostTensor<ElementSFD, cutlass::layout::PackedVectorLayout> block_SFD;          //fp8
 // Reference Output Tensors
 cutlass::HostTensor<ElementD, cutlass::layout::PackedVectorLayout> block_reference_D;
-cutlass::HostTensor<ElementSFD, cutlass::layout::PackedVectorLayout> block_reference_SFD;
-// Matrix-wide normalization constant
-cutlass::HostTensor<ElementCompute, cutlass::layout::PackedVectorLayout> block_Normconst; //fp32
+
+
 
 #endif // defined(CUTLASS_ARCH_MMA_SM100_SUPPORTED)
 
@@ -302,8 +290,6 @@ void initialize(const Options &options) {
   using namespace cute;
   // For SFA and SFB tensors layouts
   using Sm1xxBlkScaledConfig =  typename Gemm::GemmKernel::CollectiveMainloop::Sm1xxBlkScaledConfig;
-  // For SFD tensor layout
-  using Sm1xxBlockScaledOutputConfig=  typename Gemm::GemmKernel::CollectiveMainloop::Sm1xxBlkScaledConfig;
 
   stride_A = cutlass::make_cute_packed_stride(StrideA{}, {options.m, options.k, 1});
   stride_B = cutlass::make_cute_packed_stride(StrideB{}, {options.n, options.k, 1});
@@ -316,26 +302,21 @@ void initialize(const Options &options) {
   layout_D = make_layout(make_shape(options.m, options.n, 1), stride_D);
   layout_SFA = Sm1xxBlkScaledConfig::tile_atom_to_shape_SFA(cute::make_shape(options.m, options.n, options.k, 1));
   layout_SFB = Sm1xxBlkScaledConfig::tile_atom_to_shape_SFB(cute::make_shape(options.m, options.n, options.k, 1));
-  layout_SFD = SfdOutputCfg::tile_atom_to_shape_SFD(cute::make_shape(options.m, options.n, options.k, 1));
 
   block_A.reset(cutlass::make_Coord(size(layout_A)));
   block_B.reset(cutlass::make_Coord(size(layout_B)));
   block_C.reset(cutlass::make_Coord(size(layout_C)));
   block_D.reset(cutlass::make_Coord(size(layout_D)));
   block_reference_D.reset(cutlass::make_Coord(size(layout_D)));
-  block_reference_SFD.reset(cutlass::make_Coord(size(filter_zeros(layout_SFD))));
-  block_Normconst.reset(cutlass::make_Coord(1));
 
   block_SFA.reset(cutlass::make_Coord(size(filter_zeros(layout_SFA))));
   block_SFB.reset(cutlass::make_Coord(size(filter_zeros(layout_SFB))));
-  block_SFD.reset(cutlass::make_Coord(size(filter_zeros(layout_SFD))));
 
   initialize_block(block_A.host_view(), seed + 2021);
   initialize_block(block_B.host_view(), seed + 2022);
   initialize_block(block_C.host_view(), seed + 2023);
   initialize_block(block_SFA.host_view(), seed + 2024);
   initialize_block(block_SFB.host_view(), seed + 2025);
-  block_Normconst.at(cutlass::make_Coord(0)) = 2;
 
   block_A.sync_device();
   block_B.sync_device();
@@ -343,8 +324,6 @@ void initialize(const Options &options) {
   block_D.sync_device();
   block_SFA.sync_device();
   block_SFB.sync_device();
-  block_SFD.sync_device();
-  block_Normconst.sync_device();
 
 }
 
@@ -365,11 +344,6 @@ typename Gemm::Arguments args_from_options(const Options &options)
       block_C.device_data(), stride_C,
       block_D.device_data(), stride_D}
   };
-
-  if constexpr (IsBlockScaleSupported) {
-    arguments.epilogue.thread.block_scale_factor_ptr = block_SFD.device_data();
-    arguments.epilogue.thread.norm_constant_ptr      = block_Normconst.device_data();
-  }
 
   arguments.scheduler.max_swizzle_size = options.swizzle;
   return arguments;
@@ -394,7 +368,6 @@ bool verify(const Options &options) {
 
   Tensor tensor_C = cute::make_tensor(make_iterator(block_C.host_data()), layout_C);
   Tensor tensor_D = cute::make_tensor(make_iterator(block_reference_D.host_data()), layout_D);
-  Tensor tensor_SFD = make_tensor(block_reference_SFD.host_data(), layout_SFD);
 
   cutlass::reference::host::GettBlockScalingEpilogueParams<
       ElementCompute,                       // ElementScalar
@@ -402,25 +375,17 @@ bool verify(const Options &options) {
       ElementCompute,                       // ElementCompute
       decltype(tensor_C),                   // TensorC
       decltype(tensor_D),                   // TensorD
-      decltype(tensor_SFD),                 // TensorSfD
-      cute::Int<OutputSFVectorSize>,
-      cutlass::reference::host::SfStrategy::SfDGen
-    > epilogue_params {options.alpha, options.beta, tensor_C, tensor_D, tensor_SFD, block_Normconst.at(cutlass::make_Coord(0))};
+    > epilogue_params {options.alpha, options.beta, tensor_C, tensor_D};
 
   cutlass::reference::host::Gemm3x(mainloop_params, epilogue_params);
 
   // Comparison
   block_D.sync_host();
   bool passed = cutlass::reference::host::TensorEquals(block_reference_D.host_view(), block_D.host_view());
-  passed &= (cutlass::reference::host::TensorNorm(block_reference_D.host_view()) > 0);
+  passed &= (cutlass::reference::host::TensorNorm(block_reference_D.host_view()) > 0);   //确保至少有一个元素非0，范数大于0
   passed &= (cutlass::reference::host::TensorNorm(block_D.host_view()) > 0);
 
-  block_SFD.sync_host();
-  bool passed_sfd = cutlass::reference::host::TensorEquals(block_reference_SFD.host_view(), block_SFD.host_view());
-  passed_sfd &= (cutlass::reference::host::TensorNorm(block_reference_SFD.host_view()) > 0);
-  passed_sfd &= (cutlass::reference::host::TensorNorm(block_SFD.host_view()) > 0);
-
-  return passed && passed_sfd;
+  return passed;
 }
 
 /// Execute a given example GEMM computation
