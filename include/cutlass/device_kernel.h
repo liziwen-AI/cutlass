4e90baf63b7d5445600c8f3b60dfb2e20d84ba5d
liz <lzw20160910@sina.com>
2026-08-19 23:11:23 +0800

    ...

--- include/cutlass/device_kernel.h
+++ include/cutlass/device_kernel.h
@@ -4,12 +4,23 @@
 #include <cutlass/arch/synclog.hpp>  // cutlass::arch::synclog_*
 #include <cutlass/platform/platform.h> // uint64_t
 
+// __grid_constant__ was introduced in CUDA 11.7.
+#if ((__CUDACC_VER_MAJOR__ >= 12) || ((__CUDACC_VER_MAJOR__ == 11) && (__CUDACC_VER_MINOR__ >= 7))) && !CUTLASS_CLANG_CUDA
 #  define CUTLASS_GRID_CONSTANT_SUPPORTED
+#endif
 
 // __grid_constant__ can be enabled only on SM70+
+#if defined(CUTLASS_GRID_CONSTANT_SUPPORTED) && defined(__CUDA_ARCH__) && (__CUDA_ARCH__ >= 700)
 #  define CUTLASS_GRID_CONSTANT_ENABLED
-#  define CUTLASS_GRID_CONSTANT __grid_constant__
+#endif
 
+#if ! defined(CUTLASS_GRID_CONSTANT)
+#  if defined(CUTLASS_GRID_CONSTANT_ENABLED)
+#    define CUTLASS_GRID_CONSTANT __grid_constant__
+#  else
+#    define CUTLASS_GRID_CONSTANT
+#  endif
+#endif
 
 ////////////////////////////////////////////////////////////////////////////////
 
@@ -37,8 +48,7 @@ void Kernel(typename Operator::Params params) {
   Operator op;
 
   op(params, *shared_storage);
-  cutlass::arch::synclog_print();   // 只在debug的时候生效
-}
+  cutlass::arch::synclog_print();   // ???debug????????}
 
 
 /// Generic CUTLASS kernel template.
@@ -66,7 +76,10 @@ void Kernel2(typename Operator::Params params) {
 /// Generic CUTLASS kernel template.
 template <typename Operator>
 CUTLASS_GLOBAL
+#ifdef __CUDACC__
+// Enclosing this in __CUDACC__ suppresses MSVC warnings.
 __launch_bounds__(Operator::MaxThreadsPerBlock, Operator::MinBlocksPerMultiprocessor)
+#endif // __CUDACC__
 void device_kernel(CUTLASS_GRID_CONSTANT typename Operator::Params const params)
 {
   // Dynamic shared memory base pointer
