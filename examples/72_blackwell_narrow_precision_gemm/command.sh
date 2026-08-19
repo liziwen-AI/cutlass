830df4813ab6d4457172eedbe113cc239a6726f7
liz <lzw20160910@sina.com>
2026-08-19 10:30:28 +0800

    ...

--- examples/72_blackwell_narrow_precision_gemm/command.sh
+++ examples/72_blackwell_narrow_precision_gemm/command.sh
@@ -1,6 +1,7 @@
 
 
-sudo apt update && sudo apt install -y cmake make build-essential git
+# sudo apt update && sudo apt install -y cmake make build-essential git
+
 sudo apt-get install -y cuda-toolkit-13-0
 export PATH=/usr/local/cuda-13.0/bin${PATH:+:${PATH}}
 export LD_LIBRARY_PATH=/usr/local/cuda-13.0/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
@@ -8,7 +9,7 @@ export CUDA_HOME=/usr/local/cuda-13.0
 nvcc --version
 nvidia-smi
 
-
+cd ..
 git clone https://github.com/liziwen-AI/cutlass.git
 cd cutlass
 git checkout -f liz
@@ -27,4 +28,4 @@ cmake --build .\
 
 
 sudo /usr/local/cuda-13.0/bin/ncu --target-processes all \
-    ./examples/72_blackwell_narrow_precision_gemm/72b_blackwell_nvfp4_nvfp4_gemm
+./examples/72_blackwell_narrow_precision_gemm/72b_blackwell_nvfp4_nvfp4_gemm
