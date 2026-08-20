4cc1c098a15803cb72a65ea8196d370eef62693b
liz <lzw20160910@sina.com>
2026-08-20 10:44:18 +0800

    ...

--- include/cutlass/kernel_launch.h
+++ include/cutlass/kernel_launch.h
@@ -7,25 +7,14 @@
 
 namespace cutlass {
 
-///////////////////////////////////////////////////////////////////////////////////////////////////
 
-/// Structure containing the basic launch configuration of a CUDA kernel.
 struct KernelLaunchConfiguration {
 
-  /// CUDA grid dimensions
+
   dim3 grid;
-
-  /// CUDA threablock dimensions
   dim3 block;
-
-  /// Bytes of dynamically allocated SMEM in addition to static SMEM
   size_t dynamic_smem;
 
-  //
-  // Methods
-  //
-
-  /// Constructs a KernellaunchConfiguration object
   CUTLASS_HOST_DEVICE
   KernelLaunchConfiguration(
     dim3 _grid = dim3(1,1,1),
@@ -46,63 +35,14 @@ Status kernel_launch(
     cudaStream_t cuda_stream,
     const Params &kernel_params,
     bool launch_with_pdl) {
-#if (CUTLASS_DEBUG_TRACE_LEVEL > 1)
-  CUTLASS_TRACE_HOST("cutlass::kernel_launch");
-#endif
 
-  if (not launch_with_pdl) {
-#if (CUTLASS_DEBUG_TRACE_LEVEL > 1)
-    CUTLASS_TRACE_HOST("cutlass::kernel_launch: No PDL");
-#endif
     device_kernel<GemmKernel><<<grid_dims, block_dims, smem_size, cuda_stream>>>(kernel_params);
-  }
-  else {
-#if ((__CUDACC_VER_MAJOR__ >= 12) || ((__CUDACC_VER_MAJOR__ == 11) && (__CUDACC_VER_MINOR__ >= 8)))
-    if constexpr (GemmKernel::ArchTag::kMinComputeCapability < 90) {
-      CUTLASS_TRACE_HOST("  Programmatic dependent launch (PDL) is only supported for SM90.");
-      return Status::kInvalid;
-    }
 
-    cudaLaunchConfig_t config;
-    cudaLaunchAttribute attrs[1];
-
-    config.gridDim = grid_dims;
-    config.blockDim = block_dims;
-    config.dynamicSmemBytes = smem_size;
-    config.stream = cuda_stream;
-
-    config.attrs = attrs;
-    attrs[0].id = cudaLaunchAttributeProgrammaticStreamSerialization;
-    attrs[0].val.programmaticStreamSerializationAllowed = 1;
-    config.numAttrs = 1;
-
-#if (CUTLASS_DEBUG_TRACE_LEVEL > 1)
-    CUTLASS_TRACE_HOST("cutlass::kernel_launch: Calling cudaLaunchKernelEx");
-#endif
-    cudaError_t launch_result = cudaLaunchKernelEx(&config, &device_kernel<GemmKernel>, kernel_params);
-    if (cudaSuccess != launch_result) {
-      CUTLASS_TRACE_HOST("cutlass::kernel_launch: cudaLaunchKernelEx failed with error: " << cudaGetErrorString(launch_result));
-      return Status::kErrorInternal;
-    }
-#else
-    CUTLASS_TRACE_HOST("  Programmatic dependent launch (PDL) is only supported starting CUDA 11.8.");
-    return Status::kInvalid;
-#endif
-  }
-
-  cudaError_t result = cudaGetLastError();
-  if (cudaSuccess == result) {
-#if (CUTLASS_DEBUG_TRACE_LEVEL > 1)
-    CUTLASS_TRACE_HOST("cutlass::kernel_launch: cudaGetLastError reports success");
-#endif
     return Status::kSuccess;
   }
-  else {
-    CUTLASS_TRACE_HOST("  Kernel launch failed. Reason: " << result);
-    return Status::kErrorInternal;
-  }
+ 
 }
 
-///////////////////////////////////////////////////////////////////////////////////////////////////
+
 
 } // namespace cutlass
