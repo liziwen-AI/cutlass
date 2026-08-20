9bb5b25b7893a8d53b434934d0f5c2f8df7a1968
liz <lzw20160910@sina.com>
2026-08-20 11:41:35 +0800

    ...

--- include/cutlass/kernel_launch.h
+++ include/cutlass/kernel_launch.h
@@ -7,23 +7,6 @@
 
 namespace cutlass {
 
-struct KernelLaunchConfiguration {
-
-  dim3 grid;
-  dim3 block;
-  size_t dynamic_smem;
-
-  CUTLASS_HOST_DEVICE
-  KernelLaunchConfiguration(
-    dim3 _grid = dim3(1,1,1),
-    dim3 _block = dim3(1,1,1),
-    size_t _dynamic_smem = 0
-  ):
-    grid(_grid),
-    block(_block),
-    dynamic_smem(_dynamic_smem) { }
-};
-
 
 template <typename GemmKernel, typename Params>
 Status kernel_launch(
@@ -33,14 +16,8 @@ Status kernel_launch(
     cudaStream_t cuda_stream,
     const Params &kernel_params,
     bool launch_with_pdl) {
-
     device_kernel<GemmKernel><<<grid_dims, block_dims, smem_size, cuda_stream>>>(kernel_params);
-
-
-  
- 
     return Status::kSuccess;
-  
   
 }
 
